050234789a29d4e43d58ba0852a75d4e129bbf9f
Suraj <Suraj.KU@likewize.com>
2024-07-06 16:52:31 +0530

    first commit

---
+++ src/main/java/seleniumPractise/Practise01.java
@@ -0,0 +1,25 @@
+package seleniumPractise;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Practise01 {
+
+	
+	public static void main(String[] args) {
+		
+		String browser = "Chrome";
+		
+		switch(browser){		
+			case "Chrome" :
+				new ChromeDriver();
+				break;
+			case "Firefox" :
+				new FirefoxDriver();
+				break;
+
+		}
+
+	}
+
+}
